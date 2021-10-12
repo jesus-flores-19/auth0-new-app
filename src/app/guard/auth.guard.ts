7fab7ae954b73f16d95aab5792b42befac23a091
Jesus Arath <jesus.flores.p19@gmail.com>
2021-10-11 22:57:22 -0500

    Finalizamos con estructura del guard

--- src/app/guard/auth.guard.ts
+++ src/app/guard/auth.guard.ts
@@ -1,15 +1,28 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from '@auth0/auth0-angular';
+import { Router} from "@angular/router"
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+  constructor(public auth: AuthService, public router: Router){}
+  activado: any;
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+    state: RouterStateSnapshot): boolean{
+    this.auth.isAuthenticated$.subscribe((data: boolean)=>{
+      this.activado = data
+    })
+    if(this.activado){
+      return true
+    }else{
+      this.router.navigate([""])
+      return false
+    }
   }
   
 }
